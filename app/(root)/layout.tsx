999118d06f90e192d0e2db4cfac668d24852c2e8
tarunreddybabi <s.sravankumar@enterpi.com>
2025-03-26 19:38:43 +0530

    home page changes

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -14,10 +14,9 @@ const Layout = async ({ children }: { children: ReactNode }) => {
       <nav>
         <Link href="/" className="flex items-center gap-2">
           <Image src="/logo.svg" alt="MockMate Logo" width={38} height={32} />
-          <h2 className="text-primary-100">PrepWise</h2>
+          <h2 className="text-primary-100">InterviewMastery</h2>
         </Link>
       </nav>
-
       {children}
     </div>
   );
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,32 @@
+import { Button } from '@/components/ui/button'
+import { Link } from 'lucide-react'
+import Image from 'next/image'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>Home Page</div>
+    <>
+    <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
+          <p className="text-lg">
+            Practice real interview questions & get instant feedback
+          </p>
+
+          <Button asChild className="btn-primary max-sm:w-full">
+            <Link href="/interview">Start an Interview</Link>
+          </Button>
+        </div>
+
+        <Image
+          src="/robot.png"
+          alt="robo-dude"
+          width={400}
+          height={400}
+          className="max-sm:hidden"
+        />
+      </section>
+    </>
   )
 }
 
