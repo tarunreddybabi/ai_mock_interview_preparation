c00fa81dc3ba59496e74da863b8541a2c2bb605f
tarunreddybabi <147465063+tarunreddybabi@users.noreply.github.com>
2026-09-22 11:48:35 +0530

    Fix import statement for Mona_Sans font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import { Toaster } from "sonner";
 import type { Metadata } from "next";
-import { Mona_Sans } from "next/font/google";
+import { Mona_Sans } from "next/font/google"; 
 
 import "./globals.css";
 
